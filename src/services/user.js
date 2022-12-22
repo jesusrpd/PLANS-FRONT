e3265c2fbba71a2810b940310d59a0ce0f26b218
jesusrpd <=>
2022-12-21 22:31:22 -0600

    enviroment .env

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001'
+const baseUrl = process.env.REACT_APP_URI_API
 
 const login = async credentials => {
+    console.log(baseUrl)
     const res = await axios.post(`${baseUrl}/users/signin`, credentials)
     return res.data
 }
