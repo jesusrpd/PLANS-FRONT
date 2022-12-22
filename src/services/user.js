1b295a6e62144af4c041f8e6deb313f7998cb583
jesusrpd <=>
2022-12-21 22:53:14 -0600

    enviroment

--- src/services/user.js
+++ src/services/user.js
@@ -4,7 +4,14 @@ const baseUrl = process.env.REACT_APP_URI_API
 const login = async credentials => {
     console.log(baseUrl)
     const res = await axios.post(`${baseUrl}/users/signin`, credentials)
-    return res.data
+    // fetch(`${baseUrl}/users/signin`,{
+    //     method: "POST",
+    //     body: credentials
+    // }).then(response => {
+    //     console.log(response);
+    // }).catch(err => console.log(err))
+        return res.data
+        
 }
 
 export default {login}
